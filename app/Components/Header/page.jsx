608e34ee40ac4981ffa75cc0492c17898dabb1e9
narminmammadov1 <nerminytb89@gmail.com>
2025-08-21 16:32:19 +0400

    added

--- app/Components/Header/page.jsx
+++ app/Components/Header/page.jsx
@@ -317,8 +317,7 @@ const Header = () => {
         <label htmlFor="">Bakı şəhəri Sabunçu rayonu</label>  </div>
         <div className='flex gap-1'>  <input type="checkbox" />
         <label htmlFor="">Bakı şəhəri Suraxanı rayonu</label>  </div>
-        <div className='flex gap-1'>  <input type="checkbox" />
-        <label htmlFor="">Bakı şəhəri Suraxanı rayonu</label>  </div>
+       
         
       
     </div>
